0bc3c560f9f0f61d696de3118c1a99d0d7b9e103
Robert Garcia <robertserranogar@gmail.com>
2023-10-24 20:27:29 -0600

    initialized server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public")); // Serve static files from a 'public' directory
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    console.log("http://localhost:3000")
+});
